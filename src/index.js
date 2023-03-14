ffd1b1f0995589af39784a09f107b8e97c19d986
AndrewMoskvinKyiv <moskvinavkyiv@gmail.com>
2023-03-14 20:45:45 +0300

    mod:video_player

--- src/index.js
+++ src/index.js
@@ -26,9 +26,9 @@ import {router} from "./routes/routes";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
+
         <RouterProvider router={router} />
-    </React.StrictMode>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
