a70b66b6077d419ecd6fb0ef8c0a3ce0bbdb97d9
AndrewMoskvinKyiv <moskvinavkyiv@gmail.com>
2023-08-29 04:38:55 +0300

    project order

--- src/API/WebsiteApi.js
+++ src/API/WebsiteApi.js
@@ -5,7 +5,7 @@ const session = axios.create();
 
 export const WebsiteAPI = {
     getAllProjects() {
-        return session.get('https://apextest12.b-cdn.net/apex_data/apexProjects15.08.2023.json').then(response => {
+        return session.get('https://apextest12.b-cdn.net/apex_data/apexProjects29.08.2023.json').then(response => {
             return response.data
         })
     },
